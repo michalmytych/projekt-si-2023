fb4a5c70ee144030c497e001dae1baa1b8a96f35
mike <michamytych@gmail.com>
2023-06-29 18:26:08 +0200

    Added HomeController to redirect from empty url (/)

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Home controller.
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class HomeController.
+ */
+#[Route('/')]
+class HomeController extends AbstractController
+{
+    public const HOME_ROUTE = 'article_index';
+
+    /**
+     * Index action.
+     *
+     * @return RedirectResponse HTTP response
+     */
+    #[Route(
+        name: 'home_index',
+        methods: 'GET'
+    )]
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute(self::HOME_ROUTE);
+    }
+}
